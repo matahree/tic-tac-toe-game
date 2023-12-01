d96e1ce940c3b68174a7688ad13c8a92c8c9d624
Mirac <m.e.gulbeyaz@gmail.com>
2023-12-01 23:40:40 +0100

    done

--- src/App.js
+++ src/App.js
@@ -6,16 +6,36 @@ function Square({ value, onSquareClick }) {
 
 // Array(9).fill(null) creates an array with nine elements and sets each of them to null
 export default function Board() {
+    const [xIsNext, setXIsNext]= useState(true);
     const [squares, setSquares]= useState(Array(9).fill(null))
 
     function handleClick(u){
+        if(squares[u] || calculateWinner(squares)){
+            return;
+        }
         const nextSquares = squares.slice();
-        nextSquares[u] = "X";
+        if(xIsNext){
+            nextSquares[u] = "X"
+        }
+        else {
+            nextSquares[u] = "O"
+        }
         setSquares(nextSquares);
+        setXIsNext(!xIsNext);
     }
+
+    const winner = calculateWinner(squares);
+    let status;
+    if (winner) {
+        status = "Winner is: " + winner;
+    } else {
+        status = "Next player is: " + (xIsNext ? "X" : "O");
+    }
+
     // () => is an arrow function which is a shorter way to define functions
     return (
         <>
+            <div className="status">{status}</div>
             <div className="board-row">
                 <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
                 <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
@@ -33,4 +53,27 @@ export default function Board() {
             </div>
         </>
     );
+
+    function calculateWinner(squares) {
+        const lines = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ];
+        for (let i = 0; i < lines.length; i++) {
+            const [a, b, c] = lines[i];
+            if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+                return squares[a];
+            }
+        }
+        return null;
+    }
 }
+
+// NOTE: In React, it’s conventional to use onSomething names for props which represent events
+// and handleSomething for the function definitions which handle those events.
